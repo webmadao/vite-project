c600e6c2829eeecfeabde646ccd92fc8f2d5353d
Ilyes Kadji <web.kin.dev@gmail.com>
2024-09-26 17:04:58 +0200

    div dans <a>

--- src/Card.jsx
+++ src/Card.jsx
@@ -7,19 +7,20 @@ function Card ({author, story_title, title, story_url, url, _tags}) {
     // console.log(comment_text)
 
     return(
-        <div className="card">
+        <>
             
             <a className="link" href={story_url ? story_url : url} target="_blank">
+                <div className="card">
                 <figure>
                  <h1>{story_title ? story_title : title}</h1>
                     <figcaption>
                         <p>{author}, {_tags[2]}</p>
                     </figcaption>
-                    
                 </figure>
+                </div>
             </a>
-
-        </div>
+        </>
+        
     )
 }
 
